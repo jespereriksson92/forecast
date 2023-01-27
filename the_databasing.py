d13bedcdd5de24c51bd7c5d7da1ab6af0e6b0964
jespereriksson92 <erjesper@gmail.com>
2023-01-27 10:38:59 +0100

    Finalized insert-query

--- the_databasing.py
+++ the_databasing.py
@@ -1,4 +1,4 @@
-#Left to do: Create final version of database and query
+#Complete
 
 import json
 import psycopg2
@@ -17,10 +17,8 @@ def to_database():
         password="5731",
         dbname="weather")
 
-    map_weathercode = {0: 'Clear', 1:'Mainly clear',2:'Partly cloudy',3:'overcast',51:'Light drizzle',53:'Moderate drizzle',
+    map_weathercode = {0: 'Clear', 1:'Mainly clear',2:'Partly cloudy',3:'Overcast',51:'Light drizzle',53:'Moderate drizzle',
     71:'Slight snowfall',73:'Moderate snowfall',75:'Heavy snowfall'}
-
-    
 
     cur = conn.cursor()
 
@@ -28,11 +26,11 @@ def to_database():
         time = data[i]['time'].replace('T','-')
         weathercode = map_weathercode[data[i]['weathercode']]
         temperature_2m = data[i]['temperature_2m']
+        apparent_temperature = data[i]['apparent_temperature']
         winddirection_10m = data[i]['winddirection_10m']
         windspeed_10m = data[i]['windspeed_10m']
-        apparent_temperature = data[i]['apparent_temperature']
-        cur.execute("INSERT INTO wctemptest (time, temperature, weathercode, wind_direction, wind_speed,apparent_temperature) VALUES (to_timestamp(%s, 'YYYY-MM-DD-HH24:MI'), %s, %s, %s, %s,%s)", 
-        (time, temperature_2m, weathercode,winddirection_10m, windspeed_10m, apparent_temperature))
+        cur.execute("INSERT INTO one_day_forecast (time, temperature, apparent_temperature, weather_code, wind_direction, wind_speed) VALUES (to_timestamp(%s, 'YYYY-MM-DD-HH24:MI'), %s, %s, %s, %s,%s)", 
+        (time, temperature_2m, apparent_temperature, weathercode, winddirection_10m, windspeed_10m))
     conn.commit()
 
     cur.close()
